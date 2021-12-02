5f1356d745dd3a01138391d65053ca36280e2bd5
Antonio Andelic <antonio.andelic@memgraph.io>
2021-12-02 10:10:43 +0100

    Short solution

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,26 +1,11 @@
 use std::fs;
 use std::io;
 
-fn part1(lines: &[i32]) -> usize {
-    lines.windows(2).filter(|value| value[1] > value[0]).count()
-}
-
-fn part2(lines: &[i32]) -> i32 {
-    let mut current_sum: i32 = lines.iter().take(3).sum();
+fn solution(lines: &[i32], window_size: usize) -> usize {
     lines
-        .iter()
-        .zip(lines.iter().skip(3))
-        .fold(0, |mut count, (first, second)| {
-            let next_sum = current_sum - first + second;
-
-            if next_sum > current_sum {
-                count += 1;
-            }
-
-            current_sum = next_sum;
-
-            return count;
-        })
+        .windows(window_size + 1)
+        .filter(|value| value[window_size] > value[0])
+        .count()
 }
 
 fn main() -> io::Result<()> {
@@ -30,7 +15,7 @@ fn main() -> io::Result<()> {
         .map(|line| line.parse::<i32>().unwrap())
         .collect();
 
-    println!("Part1 result {}", part1(&lines));
-    println!("Part2 result {}", part2(&lines));
+    println!("Part1 result {}", solution(&lines, 1));
+    println!("Part2 result {}", solution(&lines, 3));
     Ok(())
 }
