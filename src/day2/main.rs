db7c6bb0df66f22e6e89d977afd653ea4d1ac85d
Antonio Andelic <antonio.andelic@memgraph.io>
2021-12-02 10:45:23 +0100

    Add day2 solution

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("test2");
+use std::fs;
+use std::io;
+
+enum Direction {
+    Up(i32),
+    Down(i32),
+    Forward(i32),
+}
+
+fn part1(directions: &[Direction]) -> i32 {
+    let (horizontal, vertical) = directions.iter().fold(
+        (0, 0),
+        |(horizontal, vertical), directions| match directions {
+            Direction::Up(value) => (horizontal, vertical - value),
+            Direction::Down(value) => (horizontal, vertical + value),
+            Direction::Forward(value) => (horizontal + value, vertical),
+        },
+    );
+
+    horizontal * vertical
+}
+
+fn part2(directions: &[Direction]) -> i32 {
+    let (horizontal, vertical, _aim) = directions.iter().fold(
+        (0, 0, 0),
+        |(horizontal, vertical, aim), directions| match directions {
+            Direction::Up(value) => (horizontal, vertical, aim - value),
+            Direction::Down(value) => (horizontal, vertical, aim + value),
+            Direction::Forward(value) => (horizontal + value, vertical + aim * value, aim),
+        },
+    );
+
+    horizontal * vertical
+}
+
+fn main() -> io::Result<()> {
+    let file_content = fs::read_to_string("res/day2/input.txt")?;
+    let directions: Vec<Direction> = file_content
+        .lines()
+        .map(|line| {
+            let mut words = line.split_whitespace();
+            let direction_string = words.next().unwrap();
+            let value = words.next().unwrap().parse::<i32>().unwrap();
+            assert_eq!(None, words.next());
+            match direction_string {
+                "up" => Direction::Up(value),
+                "down" => Direction::Down(value),
+                "forward" => Direction::Forward(value),
+                other => panic!("Invalid value for direction {}", other),
+            }
+        })
+        .collect();
+
+    println!("Part1 result {}", part1(&directions));
+    println!("Part2 result {}", part2(&directions));
+
+    Ok(())
 }
